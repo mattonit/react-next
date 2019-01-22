3f93331483654fb66ed975bc35a62bbe68bf7fa2
Mattonit <bartlomiej.malanowski@hotmail.com>
2019-01-22 21:41:34 +0100

    updated link in header

--- components/header.js
+++ components/header.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import Link from 'next/link';
 import styled from 'styled-components'
 
 const Navbar = styled.nav`
+  display: flex;
   width: 100%;
   background: #fff;
   border-bottom: 1px solid #ededed;
@@ -12,6 +14,13 @@ const Logo = styled.p`
   font-weight: 500;
 `
 
+const Nav = styled.ul`
+  display: flex;
+  margin-left: auto;
+  padding-left: 0;
+  list-style: none;
+`
+
 class Header extends Component {
 
   render() {
@@ -19,6 +28,14 @@ class Header extends Component {
     return(
       <Navbar>
         <Logo>Navbar Brand</Logo>
+        <Nav>
+        	<Link href="/">
+        		<a>Home</a>
+          </Link>
+        	<Link href="/about">
+            <a>About</a>
+          </Link>
+        </Nav>
       </Navbar>
     );
   }
